f5253c48e1311a3a8280f22c182490725e831943
Tem Pearse <tem.pearse01@gmail.com>
2023-09-03 19:06:13 +1200

    localhost readded for editing

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+  },
+  define: {
+    'process.env.VITE_BASE_URL': JSON.stringify(
+      process.env.VITE_BASE_URL || ''
+    ),
+  },
 })
